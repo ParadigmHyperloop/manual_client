8ccc76c765155e8a8646898d2354a282ae81a434
Eddie Hurtig <eddie@hurtigtechnologies.com>
2017-07-05 00:59:29 -0700

    Initial Commit

---
+++ manual_client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  manual_client
+//
+//  Created by Eddie Hurtig on 7/5/17.
+//  Copyright © 2017 Eddie Hurtig. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ manual_client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  manual_client
+//
+//  Created by Eddie Hurtig on 7/5/17.
+//  Copyright © 2017 Eddie Hurtig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
